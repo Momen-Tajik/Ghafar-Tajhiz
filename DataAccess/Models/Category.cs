f056ec89cf82155156da345d49956e3b253bd23b
Momen <tajikmomen23@gmail.com>
2025-11-06 13:08:05 +0330

    creat category and product model

---
+++ DataAccess/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "نام دسته‌بندی الزامی است")]
+        [Display(Name = "نام دسته‌بندی")]
+        [StringLength(50, ErrorMessage = "نام دسته‌بندی نمی‌تواند بیش از 50 کاراکتر باشد")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "توضیحات")]
+        [DataType(DataType.MultilineText)]
+        public string CategoryDescription { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        
+        
+    }
+}
---
+++ DataAccess/Models/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "نام محصول الزامی است")]
+        [Display(Name = "نام محصول")]
+        [StringLength(100, ErrorMessage = "نام محصول نمی‌تواند بیش از 100 کاراکتر باشد")] 
+        public string ProductName { get; set; }
+
+        [Display(Name = "توضیحات")]
+        [DataType(DataType.MultilineText)]
+        public string ProductDescription { get; set; }
+
+        [Required(ErrorMessage = "قیمت محصول الزامی است")]
+        [Display(Name = "قیمت")]
+        [Range(0, double.MaxValue, ErrorMessage = "قیمت باید عددی مثبت باشد")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "تعداد موجودی")]
+        [Range(0, int.MaxValue, ErrorMessage = "تعداد موجودی نمی‌تواند منفی باشد")]
+        public int StockQuantity { get; set; }
+
+        [Display(Name = "آدرس تصویر")]
+        [Url(ErrorMessage = "آدرس تصویر باید یک URL معتبر باشد")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "وضعیت موجودی")]
+        public bool IsAvailable { get; set; } = true;
+
+        [Display(Name = "تاریخ ایجاد")]
+        [DataType(DataType.DateTime)]
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        // دسته‌بندی
+        [Required(ErrorMessage = "دسته‌بندی الزامی است")]
+        [Display(Name = "دسته‌بندی")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+
+    }
+}
