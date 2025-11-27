5f81de4033b61ebfa7c347758877d4b0c2b6bfff
Momen <tajikmomen23@gmail.com>
2025-11-28 00:01:41 +0330

    add file upload

---
+++ BusinessLogic/FileUpload/IFileUploadService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.FileUpload
+{
+    public interface IFileUploadService
+    {
+        Task<string> UploadFileAsync(IFormFile file);
+    }
+}
