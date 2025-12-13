6449a15d7416c3f2e13fc597e1c1e2d1bd7699cd
Momen <tajikmomen23@gmail.com>
2025-12-13 12:08:00 +0330

    add view component for HomeProducts

---
+++ Ghafar-Tajhiz/Component/HomeProducts.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ghafar_Tajhiz.Component
+{
+    public class HomeProducts:ViewComponent
+    {
+        private readonly ProductService _productService;
+        public HomeProducts(ProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string productId)
+        {
+            var data = await _productService.GetProductsWithCategory();
+            return View("/Views/Shared/Component/HomeProduct.cshtml",data);
+        }
+    }
+}
