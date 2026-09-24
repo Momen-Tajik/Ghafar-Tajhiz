511771f20dfeed5199f24d35e37ea4faf266b489
Momen <tajikmomen23@gmail.com>
2026-09-24 14:11:43 +0330

    ChangeCommentSystem

---
+++ DataAccess/Migrations/20260924103252_OptimizingDataAccess.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptimizingDataAccess : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Baskets_UserId",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Baskets_UserId_Status",
+                table: "Baskets",
+                columns: new[] { "UserId", "Status" },
+                unique: true,
+                filter: "[Status] = 0");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Baskets_UserId_Status",
+                table: "Baskets");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Baskets_UserId",
+                table: "Baskets",
+                column: "UserId");
+        }
+    }
+}
